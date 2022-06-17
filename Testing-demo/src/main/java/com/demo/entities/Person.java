22669c94cca83a5567f8a22956d4ee24adb8f8ec
ngactive99 <ngactive99@gmail.com>
2022-06-17 15:20:04 +0530

    first commit

---
+++ Testing-demo/src/main/java/com/demo/entities/Person.java
@@ -0,0 +1,40 @@
+package com.demo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+	 
+	@Id
+	private Integer personId;
+	private String personName;
+	private String personCity;
+	public Integer getPersonId() {
+		return personId;
+	}
+	public void setPersonId(Integer personId) {
+		this.personId = personId;
+	}
+	public String getPersonName() {
+		return personName;
+	}
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+	public String getPersonCity() {
+		return personCity;
+	}
+	public void setPersonCity(String personCity) {
+		this.personCity = personCity;
+	}
+	public Person(Integer personId, String personName, String personCity) {
+		super();
+		this.personId = personId;
+		this.personName = personName;
+		this.personCity = personCity;
+	}
+	public Person() {
+		super();
+	}
+}
---
+++ Testing-demo/src/main/java/com/demo/services/PersonService.java
@@ -0,0 +1,21 @@
+package com.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.entities.Person;
+import com.demo.repo.PersonRepo;
+
+@Service
+public class PersonService {
+	
+	@Autowired
+	private PersonRepo repo;
+	
+	public List<Person> getAllPerson() {
+		return this.repo.findAll();
+	}
+
+}
