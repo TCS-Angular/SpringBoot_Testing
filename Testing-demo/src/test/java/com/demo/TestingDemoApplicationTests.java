b0c34e15f415734efa567a29d121abc0b3b569c1
ngactive99 <ngactive99@gmail.com>
2022-06-17 17:21:07 +0530

    tested repository

--- Testing-demo/src/test/java/com/demo/TestingDemoApplicationTests.java
+++ Testing-demo/src/test/java/com/demo/TestingDemoApplicationTests.java
@@ -1,13 +1,38 @@
 package com.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
 class TestingDemoApplicationTests {
+
+	private Calculator c = new Calculator();
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	@Disabled
+	void testSum() {
+		// expected result
+		int expectedResult = 50;
+		// actual result
+		int actualResult = c.doSum(12, 15, 23);
+
+		assertThat(actualResult).isEqualTo(expectedResult);
+
+	}
+
+	@Test
+	void TestProduct() {
+		// expected result
+		int expected = 20;
+		int actual = c.doProduct(2, 10);
+
+		assertThat(actual).isEqualTo(expected);
 	}
 
 }
---
+++ Testing-demo/src/test/java/com/demo/repo/PersonRepoTest.java
@@ -0,0 +1,41 @@
+package com.demo.repo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.demo.entities.Person;
+
+@SpringBootTest
+public class PersonRepoTest {
+
+	@Autowired
+	private PersonRepo personRepo;
+
+	@Test
+	void isPersonExitsById() {
+		Person person = new Person(123, "Raghav", "Delhi");
+		personRepo.save(person);
+
+		Boolean actualResult = personRepo.isPersonExistsById(123);
+
+		assertThat(actualResult).isTrue();
+
+	}
+	
+	@AfterEach
+	void tearDown() {
+		System.out.println("tearing down ");
+		personRepo.deleteAll();
+	}
+	
+	@BeforeEach
+	void setUp() {
+		System.out.println("setting up");
+	}
+
+}
